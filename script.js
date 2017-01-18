a2bb45ecbedc17e98988f87fbd38001e49893627
Thomas Cross <thomas.corey.cross@gmail.com>
2017-01-18 23:26:31 +0000

    Smiley face

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //Write your function calls below this line
+/*global setColor*/
+setColor(0, 0, "black");
+setColor(0, 1, "black");
+setColor(0, 2, "black");
+setColor(1, 0, "black");
+setColor(1, 2, "black");
+setColor(2, 0, "black");
+setColor(2, 1, "black");
+setColor(2, 2, "black");
+
+setColor(0, 7, "black");
